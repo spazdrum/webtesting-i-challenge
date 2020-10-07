7c8f61e9bfbfc91b89a6a73990337a4180512a0f
spazdrum <spazdrum@gmail.com>
2020-10-06 21:36:50 -0400

    describe(repair)

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,18 @@
-const enhancer = require('./enhancer.js');
+const enhancer = require("./enhancer.js");
 // test away!
+
+describe("repair", () => {
+  it("should restore durability to 100", () => {
+    expect(
+      enhancer.repair({
+        name: "Joshua",
+        durability: 80,
+        enhancement: 10,
+      })
+    ).toEqual({
+      name: "Joshua",
+      durability: 100,
+      enhancement: 10,
+    });
+  });
+});
